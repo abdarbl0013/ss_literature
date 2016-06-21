2d99da8770e9af0326aee316222c4b343cd63d2e
Aman Mittal <aman@seynse.com>
2016-06-21 16:54:50 +0530

    first draft

--- erostory_finder/spiders/storyfinder.py
+++ erostory_finder/spiders/storyfinder.py
@@ -12,24 +12,25 @@ class StoryFinderSpider(scrapy.Spider):
     def parse(self, response):
         soup = BeautifulSoup(response.body, 'lxml')
         item = ErostoryFinderItem()
-        # search_list = ['blonde', 'black', 'boyfriend', 'girlfriend', 'bed', 'down', 'up']
+        search_list = ['blonde', 'black', 'boyfriend', 'girlfriend', 'bed', 'down', 'up', 'sofa']
         while True:
             links = soup.select('.b-story-list .w-34t')
             for link in links:
                 story_url = link.find('a', attrs={'class': 'r-34i'})
-                # res = requests.get(story_url.get('href'))
-                # story_soup = BeautifulSoup(res.text, 'lxml')
-                # pages = story_soup.select('#sbar-l-wrp .b-pager')[0]
-                # if len(pages.find_all('a')) != 0:
-                #     continue
-                #
-                # story = story_soup.find('div', attrs={'class': 'b-story-body-x'}).get_text()
-                # for key in search_list:
-                #     if key not in story:
-                #         break
-                item['name'] = story_url.string
-                item['url'] = story_url.get('href')
-                yield item
+                res = requests.get(story_url.get('href'))
+                story_soup = BeautifulSoup(res.text, 'lxml')
+                pages = story_soup.select('#sbar-l-wrp .b-pager')[0]
+                if len(pages.find_all('a')) != 0:
+                    continue
+
+                story = story_soup.find('div', attrs={'class': 'b-story-body-x'}).get_text()
+                for key in search_list:
+                    if key not in story:
+                        break
+                else:
+                    item['name'] = story_url.string
+                    item['url'] = story_url.get('href')
+                    yield item
 
             next_page = soup.find('a', text='Next')
             if next_page:
